8460a5e974c39b0d3af07d6ca58070a54147c3e7
mftfulfilment <78541297+mftfulfilment@users.noreply.github.com>
2021-09-13 12:01:04 +0300

    Mail

--- mailcode/mailer.php
+++ mailcode/mailer.php
@@ -3,69 +3,27 @@ require "./vendor/autoload.php";
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 if(isset($_POST['name'])) {
-try {
 
-    $mail = new PHPMailer();
+    $apiKey = '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';
+    // $sg = new \SendGrid($apiKey);
 
-// $mail->SMTPDebug = 2; 
-    $name =$_POST['name'];//req
-    $subject =$_POST['phone'];//req
-    $email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $service = $_POST['service'];
-//    $message = $_POST['message'];
-
-    $content = file_get_contents("./email1.html", true);
-
-    foreach ($_POST as $key => $value) {
-        $content = str_replace('{{'.$key.'}}', $value, $content);
+    $email = new \SendGrid\Mail\Mail();
+    $email->setFrom("test@example.com", "Example User");
+    $email->setSubject("Sending with Twilio SendGrid is Fun");
+    $email->addTo("support@mftfulfillmentcentre.com", "Example User");
+    $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+    $email->addContent(
+        "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+    );
+    $sendgrid = new \SendGrid($apiKey);
+    try {
+        $response = $sendgrid->send($email);
+        print $response->statusCode() . "\n";
+        print_r($response->headers());
+        print $response->body() . "\n";
+    } catch (Exception $e) {
+        echo 'Caught exception: '. $e->getMessage() ."\n";
     }
-//$message .= "Name is :"."&nbsp;".$name."<br />";
-    $message = trim($content);
 
-/*
-    $mail->IsSMTP(true);                                           // Set mailer to use SMTP
-$mail->Host = 'premium50.web-hosting.com';                          // Specify main and backup server
-$mail->Port = 465;                                                 // Set the SMTP port
-$mail->SMTPAuth = true;                                            // Enable SMTP authentication
-$mail->Username = 'mailer@noreply.mftfulfillmentcentre.com';                // SMTP username
-$mail->Password = 'YEg-!cLaURf+';                              // SMTP password
-    $mail->SMTPSecure = 'ssl';
-    */
-     $mail->IsSMTP(true);                                           // Set mailer to use SMTP
-$mail->Host = 'smtp.sendgrid.net';                          // Specify main and backup server
-$mail->Port = 465;                                                 // Set the SMTP port
-$mail->SMTPAuth = true;                                            // Enable SMTP authentication
-$mail->Username = 'apikey';                // SMTP username
-$mail->Password = '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';                              // SMTP password
-    $mail->SMTPSecure = 'ssl'; 
-    // Enable encryption, 'ssl' also accepted
-
-
-    $mail->setFrom('mailer@noreply.mftfulfillmentcentre.com', 'noreply');
-    $mail->FromName = 'MFT Fulfillment Website Query';
-
-
-//   $mail->AddAddress('samaksh@electrovese.com');
-    // $mail->addAddress(' samaksh@electrovese.com','dsds');
-    
-    $mail->addAddress('support@mftfulfillmentcentre.com','dsds');
-    $mail->IsHTML(true);                                  // Set email format to HTML
-
-    $mail->Subject = 'Query From User '. $name;
-    $mail->Body    = $message;
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-     if(!$mail->send()) {
-        echo 'Message could not be sent. ';
-        echo 'Mailer Error: ' . $mail;
-        exit;
-    }
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-}
-header("Location: /")
-?>
+header("Location: /boom");
