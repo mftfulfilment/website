16058dd9e98ffa514e3e32bc37ba95f4db8d9316
mftfulfilment <78541297+mftfulfilment@users.noreply.github.com>
2021-09-13 12:18:12 +0300

    Test

--- mailcode/mailer.php
+++ mailcode/mailer.php
@@ -4,26 +4,15 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 if(isset($_POST['name'])) {
 
-    $apiKey = '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';
-    // $sg = new \SendGrid($apiKey);
+   
+    $to      = 'support@mftfulfillmentcentre.com';
+    $subject = 'the subject';
+    $message = 'hello';
+    $headers = 'From: webmaster@example.com'       . "\r\n" .
+                 'Reply-To: webmaster@example.com' . "\r\n" .
+                 'X-Mailer: PHP/' . phpversion();
 
-    $email = new \SendGrid\Mail\Mail();
-    $email->setFrom("support@mftfulfillmentcentre.com", "Example User");
-    $email->setSubject("Sending with Twilio SendGrid is Fun");
-    $email->addTo("support@mftfulfillmentcentre.com", "Example User");
-    $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-    $email->addContent(
-        "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-    );
-    $sendgrid = new \SendGrid($apiKey);
-    try {
-        $response = $sendgrid->send($email);
-        print $response->statusCode() . "\n";
-        print_r($response->headers());
-        print $response->body() . "\n";
-    } catch (Exception $e) {
-        echo 'Caught exception: '. $e->getMessage() ."\n";
-    }
+    mail($to, $subject, $message, $headers);
 
 }
 header("Location: /");
